0f166bba60e7025238ff25ea3a9bf2e5c8c00303
jasky12 <jaskiratsingh121104@gmail.com>
2026-04-09 11:45:36 +0200

    esercizio fatoriale

---
+++ 1_Recursion/fattoriale.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+unsigned long long Fattoriale(int n) {
+    if (n < 0) {
+        return 0;
+    }
+    if (n == 0) {
+        return 1;
+    }
+    return Fattoriale(n - 1) * n;
+}
+
+/*int main(void) {
+    int n = 5;
+    unsigned long long fatt = Fattoriale(n);
+    fprintf(stdout, "%llu\n", fatt);
+    return 0;
+}*/
